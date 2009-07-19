d7ba67933bb4ec65d89f9d90493d59905d27e6ff
markusher <markusher>
2009-07-19 17:52:43 +0000

    re-structure of serial routines

---
+++ EconetBridge/avruip/drivers/serial.c
@@ -0,0 +1,100 @@
+
+
+#include <avr/io.h>
+#include <avr/sleep.h>
+#include "serial.h"
+
+extern void serial_tx(uint8_t mask);
+extern void serial_tx_hex(uint8_t mask);
+
+void serial_short(unsigned short val)
+{
+
+	unsigned short tmp;
+
+	tmp = ((val & 0xFF00) >>8);		// calc high byte
+	serial_tx_hex((unsigned char)tmp);	// output high byte in hex
+
+	serial_tx(0x20);				// space
+
+	tmp = (val & 0x00FF);			// calc lo byte
+	serial_tx_hex((unsigned char)tmp);	// output high byte in hex
+
+	return;
+
+}
+
+void serial_shortLH(unsigned short val)
+{
+
+	unsigned short tmp;
+
+	tmp = (val & 0x00FF);			// calc lo byte
+	serial_tx_hex((unsigned char)tmp);	// output high byte in hex
+
+	serial_tx(0x20);				// space
+
+	tmp = ((val & 0xFF00) >>8);		// calc high byte
+	serial_tx_hex((unsigned char)tmp);	// output high byte in hex
+
+	return;
+
+}
+
+void serial_ok(unsigned char okVal)
+{
+
+	serial_tx(0x6f);		// o
+	serial_tx(0x6b);		// k
+	serial_tx(0x20);		// space
+	serial_tx_hex(okVal);	// value
+	serial_crlf(); 		// cr lf
+
+	return;
+}
+
+void serial_rx(void)
+{
+
+	serial_tx(0x72);		// r
+	serial_tx(0x78);		// x
+	serial_tx(0x20);		// space
+	return;
+}
+
+void serial_error(unsigned char eVal)
+{
+
+	serial_tx(0x65);		// e
+	serial_tx(0x20);		// space
+	serial_tx_hex(eVal);	// error value
+	serial_crlf(); 		// cr lf
+
+	return;
+}
+
+void serial_crlf(void)
+{
+
+	serial_tx(13);		// cr
+	serial_tx(10);		// lf
+
+	return;
+}
+
+void serial_packet(unsigned short pktbuff, unsigned short pktlen)
+{
+// print out packet content
+	unsigned short c;
+	unsigned short *x;
+	x=pktbuff;
+
+  for(c=0; c < pktlen/2; c++) {	
+	serial_shortLH(*x++);
+	serial_tx(0x20);
+	}
+	serial_crlf();
+
+	return;
+}
+
---
+++ EconetBridge/avruip/drivers/serial.h
@@ -0,0 +1,35 @@
+/**
+ * \addtogroup drivers
+ * @{
+ */
+
+/**
+ * \defgroup Serial
+ * @{
+ *
+ */
+
+/**
+ * \file
+ *         Header file for Serial routines
+ *         
+ * \author
+ *         Mark Usher
+ */
+
+#ifndef __SERIAL_H__
+#define __SERIAL_H__
+
+
+void serial_short(unsigned short);
+void serial_shortLH(unsigned short);
+void serial_ok(unsigned char);
+void serial_rx(void);
+void serial_error(unsigned char);
+void serial_crlf(void);
+void serial_packet(unsigned short, unsigned short);
+
+
+#endif /* __ADLC_H__ */
+/** @} */
+/** @} */
