72c0aba0c63690d4a48472ec31b9bdce6f3c50b0
philb <philb>
2009-07-20 18:52:42 +0000

    further adlc fixups

--- EconetBridge/avruip/drivers/adlc.c
+++ EconetBridge/avruip/drivers/adlc.c
@@ -1,10 +1,7 @@
 
 #include "adlc.h"
 
-volatile unsigned char EC_BUF[2000];
-
-volatile extern unsigned char *ECONET_RX_BUF asm("ECONET_RX_BUF") = EC_BUF;
-//extern unsigned short adlc_rx_ptr;
+volatile unsigned char ECONET_RX_BUF[2000];
 
 volatile extern unsigned short adlc_rx_ptr asm("adlc_rx_ptr") = 0;
 
@@ -17,7 +14,8 @@ void	adlc_poller(void)
 	if (adlc_state == FRAME_COMPLETE)
 	{
 		// print out the packet on the serial
-		serial_packet(&ECONET_RX_BUF,0x12);
+		serial_packet(ECONET_RX_BUF,0x12);
+		adlc_ready_to_receive();
 	}
 
 	return;
