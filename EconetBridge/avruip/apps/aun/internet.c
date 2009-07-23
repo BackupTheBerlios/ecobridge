f52e57f562666bde60c07914be3d975ac15f7d2b
philb <philb>
2009-07-23 21:56:44 +0000

    correct name matching

--- EconetBridge/avruip/apps/aun/internet.c
+++ EconetBridge/avruip/apps/aun/internet.c
@@ -12,8 +12,9 @@ static unsigned char response_buffer[32];
 #define FIND_SERVER_PORT	0xb0
 #define FIND_SERVER_REPLY_PORT	0xb1
 
-#define MY_SERVER_TYPE		"INTERNET"
-#define MY_SERVER_NAME		"TCP/IP Gateway"
+static const char *MY_SERVER_TYPE = "INTERNET";
+static const char *MY_SERVER_NAME = "TCP/IP Gateway";
+static const char *WILDCARD_SERVER_TYPE = "        ";
 
 static void setup_find_server_rxcb(void)
 {
@@ -30,26 +31,24 @@ void internet_poller(void)
   uint8_t stn, net;
   if (poll_rx (find_server_rxcb, &stn, &net) == RXCB_RECEIVED)
   {
-    serial_tx_str ("find server ");
     int i;
-    for (i = 0; i < 8; i++)
-      serial_tx (bcast_buf[i]);
-    serial_tx_hex (net);
-    serial_tx_hex (stn);
-    serial_crlf();
-    response_buffer[0] = stn;
-    response_buffer[1] = net;
-    response_buffer[2] = my_station;
-    response_buffer[3] = 0;
-    response_buffer[4] = 0x80;
-    response_buffer[5] = FIND_SERVER_REPLY_PORT;
-    response_buffer[6] = 0;
-    response_buffer[7] = 0xd2;
-    response_buffer[8] = 1;
-    strcpy (response_buffer + 9, MY_SERVER_TYPE);
-    response_buffer[17] = strlen(MY_SERVER_NAME);
-    strcpy (response_buffer + 18, MY_SERVER_NAME);
-    enqueue_tx (response_buffer, 18 + strlen(MY_SERVER_NAME));
+    if (memcmp (bcast_buf, MY_SERVER_TYPE, 8) == 0
+	|| memcmp (bcast_buf, WILDCARD_SERVER_TYPE, 8) == 0)
+    {
+      response_buffer[0] = stn;
+      response_buffer[1] = net;
+      response_buffer[2] = my_station;
+      response_buffer[3] = 0;
+      response_buffer[4] = 0x80;
+      response_buffer[5] = FIND_SERVER_REPLY_PORT;
+      response_buffer[6] = 0;
+      response_buffer[7] = 0xd2;
+      response_buffer[8] = 1;
+      strcpy (response_buffer + 9, MY_SERVER_TYPE);
+      response_buffer[17] = strlen(MY_SERVER_NAME);
+      strcpy (response_buffer + 18, MY_SERVER_NAME);
+      enqueue_tx (response_buffer, 18 + strlen(MY_SERVER_NAME));
+    }
     close_rx (find_server_rxcb);
     setup_find_server_rxcb ();
   }
