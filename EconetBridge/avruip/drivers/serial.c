591240fb3f328944ce3bb810002fe1c8d231ed17
philb <philb>
2009-07-23 22:01:52 +0000

    reduce code size

--- EconetBridge/avruip/drivers/serial.c
+++ EconetBridge/avruip/drivers/serial.c
@@ -40,10 +40,7 @@ void serial_shortLH(unsigned short val)
 
 void serial_ok(unsigned char okVal)
 {
-
-	serial_tx(0x6f);		// o
-	serial_tx(0x6b);		// k
-	serial_tx(0x20);		// space
+	serial_tx_str("ok ");
 	serial_tx_hex(okVal);	// value
 	serial_crlf(); 		// cr lf
 
@@ -52,66 +49,40 @@ void serial_ok(unsigned char okVal)
 
 void serial_rx(void)
 {
-
-	serial_tx(0x72);		// r
-	serial_tx(0x78);		// x
-	serial_tx(0x20);		// space
-	return;
+	serial_tx_str("rx ");
 }
 
 void serial_txx(void)
 {
-
-	serial_tx(0x74);		// t
-	serial_tx(0x78);		// x
-	serial_tx(0x20);		// space
-	return;
+	serial_tx_str("tx ");
 }
 
 void serial_eth(void)
 {
-
-	serial_tx(0x65);		// e
-	serial_tx(0x74);		// t
-	serial_tx(0x68);		// h
-	serial_tx(0x20);		// space
-	return;
+	serial_tx_str("eth ");
 }
 
 void serial_eco(void)
 {
-
-	serial_tx(0x65);		// e
-	serial_tx('c');			// c
-	serial_tx(0x6f);		// o
-	serial_tx(0x20);		// space
-	return;
+	serial_tx_str("eco ");
 }
 
 void serial_error(unsigned char eVal)
 {
-
-	serial_tx(0x65);		// e
-	serial_tx(0x20);		// space
+	serial_tx_str("e ");
 	serial_tx_hex(eVal);	// error value
 	serial_crlf(); 		// cr lf
-
-	return;
 }
 
 void serial_crlf(void)
 {
-
-	serial_tx(13);		// cr
-	serial_tx(10);		// lf
-
-	return;
+	serial_tx_str("\r\n");
 }
 
 void serial_tx_str(char *msg)
 {
-  while (*msg)
-    serial_tx (*(msg++));
+	while (*msg)
+		serial_tx (*(msg++));
 }
 
 void serial_packet(unsigned short pktbuff, unsigned short pktlen)
