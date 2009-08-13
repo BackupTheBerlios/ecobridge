37a2623875e3c496ed0b4ff5594ef95d7e1ea7f6
philb <philb>
2009-08-13 20:13:47 +0000

    optimize egpio access a bit

--- EconetBridge/avruip/drivers/egpio.h
+++ EconetBridge/avruip/drivers/egpio.h
@@ -3,15 +3,15 @@
 // =======================================================================
 
 #define	EGPIO_ECONET_RED		0x0			// Econet Red LED
-#define	EGPIO_ECONET_GREEN	0x1			// Econet Green LED
-#define	EGPIO_STATUS_RED		0x2			// Status Red LED
-#define	EGPIO_ADLC_RESET		0x3			// ADLC Reset
-#define	EGPIO_CLOCK_ENABLE	0x4			// Econet Clock Enable
-#define	EGPIO_STATUS_GREEN	0x5			// Status Green LED
-#define	EGPIO_ETHER_RESET		0x6			// Ethernet Reset
-#define	EGPIO_TP1			0x7			// Serial TX Output
+#define	EGPIO_ECONET_GREEN	0x2			// Econet Green LED
+#define	EGPIO_STATUS_RED		0x4			// Status Red LED
+#define	EGPIO_ADLC_RESET		0x6			// ADLC Reset
+#define	EGPIO_CLOCK_ENABLE	0x8			// Econet Clock Enable
+#define	EGPIO_STATUS_GREEN	0xa			// Status Green LED
+#define	EGPIO_ETHER_RESET		0xc			// Ethernet Reset
+#define	EGPIO_TP1			0xe			// Serial TX Output
 
-#define	EGPIO_SET			0x80			// flag to set
+#define	EGPIO_SET			0x1			// flag to set
 
 #ifndef __ASSEMBLER__
 extern void egpio_init(void);
