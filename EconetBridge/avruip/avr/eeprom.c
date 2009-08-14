7091a65065c549eb4b96fdc46bb4a0315ad4067e
markusher <markusher>
2009-08-14 10:10:53 +0000

    initial versions

---
+++ EconetBridge/avruip/avr/eeprom.c
@@ -0,0 +1,46 @@
+
+#include <avr\eeprom.h>
+#include "globals.h"
+#include "eeprom.h"
+
+
+//sDefaults_t INIT EEMEM =
+sDefaults_t INIT =
+		{0xAA, EEPROM_VALID, MACHINE_VER_HIGH,MACHINE_VER_LOW,	/* Safety, Initted, VER HIGH, VER LOW */ \
+		0, 1, 0x51, 130,				/* Clock, Econet Net, Station, Ethernet Net */\
+		1, 2, 128, 10,					/* IP Address */\
+		255,255, 0, 0,					/* Subnet Mask */\
+		1,2, 128, 9,					/* Default Router */ \
+		0x00,0x00, 0xA4,				/* MAC Address 1-3 OUI Acorn */	\
+		0x33, 0x44, 0x55};				/* MAC Address 4-6 NUI */
+
+
+
+
+void EEPROM_InitCheck(void)
+{
+
+    eeGlobals.Initialised = eeprom_read_byte(EEPROM_INITTED);
+
+    if (eeGlobals.Initialised != EEPROM_VALID) {
+	    eeprom_write_block(&INIT, EEPROM_SAFETY, sizeof(INIT));
+    }
+
+}
+
+void EEPROM_ReadAll(void)
+{
+    eeprom_read_block(&eeGlobals, EEPROM_SAFETY,sizeof(eeGlobals));
+}
+
+
+void EEPROM_WriteAll(void)
+{
+    eeprom_write_block(&eeGlobals, EEPROM_SAFETY, sizeof(eeGlobals));
+}
+
+void EEPROM_main (void)
+{
+   EEPROM_InitCheck();                 // Check EEPROM is valid. If not valid, Init it
+   EEPROM_ReadAll ();                  // Get retained Info from EEPROM
+}
---
+++ EconetBridge/avruip/avr/eeprom.h
@@ -0,0 +1,50 @@
+#ifndef EEPROM_H
+#define EEPROM_H
+
+
+// PUBLIC MODULE CONSTANT DEFINITIONS
+
+enum {  EEPROM_SAFETY,			// A power-failure *CAN* corrupt this byte
+        EEPROM_INITTED,			// The EEPROM content has been Initted flag
+        EEPROM_MACHINE_VER_HIGH,	// Version numbering
+        EEPROM_MACHINE_VER_LOW,
+        EEPROM_ECONET_CLOCK, 		// Econet clock multiplier
+        EEPROM_ECONET_NETWORK,		// Econet Network number of the econet port
+        EEPROM_ECONET_STATION,    	// Econet station
+        EEPROM_ETHERNET_NETWORK,	// Econet Network number of the ethernet port
+        EEPROM_IPADDR1,			// IP Address
+        EEPROM_IPADDR2,			//
+        EEPROM_IPADDR3,			//
+        EEPROM_IPADDR4,			//
+        EEPROM_SUBNET1,			// Subnet
+        EEPROM_SUBNET2,			//
+        EEPROM_SUBNET3,			//
+        EEPROM_SUBNET4,			//
+        EEPROM_GATEWAY1,		// Gateway
+        EEPROM_GATEWAY2,		//
+        EEPROM_GATEWAY3,		//
+        EEPROM_GATEWAY4,		//
+        EEPROM_MAC1,			// MAC Address
+        EEPROM_MAC2,			//
+        EEPROM_MAC3,			//
+        EEPROM_MAC4,			//
+        EEPROM_MAC5,			//
+        EEPROM_MAC6			//
+     };
+
+
+#define EEPROM_VALID    (0x55)
+
+
+
+//  PUBLIC FUNCTION PROTOTYPES
+
+void    EEPROM_InitCheck(void);
+extern  void    EEPROM_ReadAll(void);
+extern  void    EEPROM_WriteAll(void);
+extern  void    EEPROM_main(void);
+
+#endif
+
+
+
